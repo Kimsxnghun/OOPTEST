39bfe81fe8f3b8aefee7c6271000ded2a31db4b9
Kimsunghun <rlatjdgns432@gmail.com>
2022-01-31 17:34:30 +0900

    - Junit

---
+++ OOPTest/src/Test/Test.java
@@ -0,0 +1,19 @@
+package Test;
+
+import static org.junit.Assert.*;
+
+import Main.Main;
+
+public class Test {
+
+	@org.junit.Test
+	public void testCreate() {
+		
+		Main main = new Main();
+		
+		main.create();
+		
+		fail("Not yet implemented");
+	}
+
+}
